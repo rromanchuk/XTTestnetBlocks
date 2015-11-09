745dfc563c0edd6d9f414cbdc3bc9227064b1aa3
Har01d <Alf@MacBook-Pro-Alf.local>
2015-11-09 20:21:44 +0300

    XTTestnetBlocks v.0.1A

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+// (c) Har01d
+
+const DAEMON_PROTOCOL = 'http';
+const DAEMON_LOGIN    = 'alf';
+const DAEMON_PASSWORD = 'bitcoindpasgithubswordalphalimafoxtrot';
+const DAEMON_HOST     = 'localhost';
+const DAEMON_PORT     = '8332';
+
+const POSTGRESQL_HOST     = 'localhost';
+const POSTGRESQL_DB       = 'blockchain';
+const POSTGRESQL_LOGIN    = 'Alf';
+const POSTGRESQL_PASSWORD = '';
+
+const FIRST_BLOCK_TO_PROCESS = 580000;
